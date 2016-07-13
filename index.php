022a82a8f4f960753fd684aaccc1a2e3864381b6
Federico <fedefog@hotmail.com>
2016-07-13 11:19:44 -0300

    index

---
+++ index.php
@@ -0,0 +1,219 @@
+<?php $section = 'home'; ?>
+<?php include 'header.php'; ?>
+    
+    <section id="hero">
+
+        <div class="container">
+
+                <div class="row">
+
+                    <div class="col-md-12">
+
+                        <h2><strong>Supplying light, Sound and Motion Components</strong><br> to the POS Industry for over 20 years.</h2>
+
+                        <a href="" class="btn btn-primary">VIEW PRODUCTS <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+
+                    </div><!-- / col 12 -->
+
+                </div><!-- / row -->
+
+            </div><!-- / container -->
+
+    </section>
+
+    <section id="offer">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-offset-1 col-md-10 text-center">
+                    <span class="offer-title">PRICE BREAKTHROUGH!</span>    
+                    <span class="offer-des">WHITE 5mm LEDs  -  <strong>29p PER UNIT</strong></span>
+                    <a href="" title="" class="btn btn-primary">VIEW OFFER <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+                </div><!-- / col 12 -->
+
+            </div><!-- / row -->
+
+        </div><!-- / container -->
+
+    </section>
+
+    <section id="main">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-12 text-center">
+                   <h3>Bringing life to your POS displays.</h3>
+                </div><!-- / col 12 -->
+
+            </div><!-- / row -->
+
+            <div class="row">
+
+                <div class="list-main-products">
+
+                    <div class="col-md-4">
+                        <a href="#" title="">
+                            <div class="prod">
+                                <div class="pull-left">
+                                    <h4>LED Lights Components</h4>
+                                    <p>For show-stopping, eye-catching power, look no further than LEDs from Rocket Production.</p>
+                                    <div class="btn btn-secundary">VIEW PRODUCTS <i class="fa fa-chevron-right" aria-hidden="true"></i></div>
+                                </div><!-- / col left -->
+                                <div class="pull-right">
+                                    <img src="images/prod.png" class="img-responsive" title="" alt="product image">
+                                </div><!-- / col left -->
+                            </div>
+                        </a>
+                    </div><!-- / col 4 -->
+
+                    <div class="col-md-4">
+                        <a href="#" title="">
+                            <div class="prod">
+                                <div class="pull-left">
+                                    <h4>LED Lights Components</h4>
+                                    <p>For show-stopping, eye-catching power, look no further than LEDs from Rocket Production.</p>
+                                    <div class="btn btn-secundary">VIEW PRODUCTS <i class="fa fa-chevron-right" aria-hidden="true"></i></div>
+                                </div><!-- / col left -->
+                                <div class="pull-right">
+                                    <img src="images/prod.png" class="img-responsive" title="" alt="product image">
+                                </div><!-- / col left -->
+                            </div>
+                        </a>
+                    </div><!-- / col 4 -->
+
+                    <div class="col-md-4">
+                        <a href="#" title="">
+                            <div class="prod">
+                                <div class="pull-left">
+                                    <h4>LED Lights Components</h4>
+                                    <p>For show-stopping, eye-catching power, look no further than LEDs from Rocket Production.</p>
+                                    <div class="btn btn-secundary">VIEW PRODUCTS <i class="fa fa-chevron-right" aria-hidden="true"></i></div>
+                                </div><!-- / col left -->
+                                <div class="pull-right">
+                                    <img src="images/prod.png" class="img-responsive" title="" alt="product image">
+                                </div><!-- / col left -->
+                            </div>
+                        </a>
+                    </div><!-- / col 4 -->
+
+                    <div class="col-md-4">
+                        <a href="#" title="">
+                            <div class="prod">
+                                <div class="pull-left">
+                                    <h4>LED Lights Components</h4>
+                                    <p>For show-stopping, eye-catching power, look no further than LEDs from Rocket Production.</p>
+                                    <div class="btn btn-secundary">VIEW PRODUCTS <i class="fa fa-chevron-right" aria-hidden="true"></i></div>
+                                </div><!-- / col left -->
+                                <div class="pull-right">
+                                    <img src="images/prod.png" class="img-responsive" title="" alt="product image">
+                                </div><!-- / col left -->
+                            </div>
+                        </a>
+                    </div><!-- / col 4 -->
+
+                    <div class="col-md-4">
+                        <a href="#" title="">
+                            <div class="prod">
+                                <div class="pull-left">
+                                    <h4>LED Lights Components</h4>
+                                    <p>For show-stopping, eye-catching power, look no further than LEDs from Rocket Production.</p>
+                                    <div class="btn btn-secundary">VIEW PRODUCTS <i class="fa fa-chevron-right" aria-hidden="true"></i></div>
+                                </div><!-- / col left -->
+                                <div class="pull-right">
+                                    <img src="images/prod.png" class="img-responsive" title="" alt="product image">
+                                </div><!-- / col left -->
+                            </div>
+                        </a>
+                    </div><!-- / col 4 -->
+
+                    <div class="col-md-4">
+                        <a href="#" title="">
+                            <div class="prod">
+                                <div class="pull-left">
+                                    <h4>LED Lights Components</h4>
+                                    <p>For show-stopping, eye-catching power, look no further than LEDs from Rocket Production.</p>
+                                    <div class="btn btn-secundary">VIEW PRODUCTS <i class="fa fa-chevron-right" aria-hidden="true"></i></div>
+                                </div><!-- / col left -->
+                                <div class="pull-right">
+                                    <img src="images/prod.png" class="img-responsive" title="" alt="product image">
+                                </div><!-- / col left -->
+                            </div>
+                        </a>
+                    </div><!-- / col 4 -->
+
+                    </div><!-- / list products -->
+
+                </div><!-- / row -->
+
+                <div class="row actions-container">
+
+                    <div class="main-actions">
+
+                        <div class="col-md-6">
+                            <div class="banner-action catalogue">
+                                <div class="title-action">
+                                    <strong>2016</strong>Product Catalogue 
+                                </div>
+                                <a href="" title="" class="btn">DOWNLOAD</a>
+                            </div>
+                        </div><!-- / col 6 -->
+
+                        <div class="col-md-6">
+                            <div class="banner-action request">
+                                <div class="title-action">
+                                    <strong>Request</strong>a call back
+                                </div>
+                                <a href="" title="" class="btn">GO</a>
+                            </div>
+                        </div><!-- / col 6 -->
+
+                    </div><!-- / main actions -->
+
+                </div><!-- / row -->
+
+        </div><!-- / container -->
+
+    </section>
+
+    <section id="usp">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-offset-2 col-md-8 text-center title-usp">
+                    We've been providing solutions to all of your POS display componentry needs since 1987.
+                </div>
+
+                <div class="col-md-4 text-center icon">
+                    <img src="images/rp-icons-01.png" class="img-responsive center-block" title="" alt="usp image">
+                    <strong>Best<br>Prices.</strong>
+                    <span class="right-arrow"></span>
+                </div><!-- / col 4 -->
+
+                <div class="col-md-4 text-center icon">
+                    <img src="images/rp-icons-02.png" class="img-responsive center-block" title="" alt="usp image">
+                    <strong>Exceptional<br>Customer Service</strong>
+                    <span class="right-arrow"></span>
+                </div><!-- / col 4 -->
+
+                <div class="col-md-4 text-center icon">
+                    <img src="images/rp-icons-03.png" class="img-responsive center-block" title="" alt="usp image">
+                    <strong>Fast<br>Delivery.</strong>
+                </div><!-- / col 4 -->
+
+                <div class="col-md-12 text-center">
+                    <a href="" title="" class="btn">LEARN MORE</a>
+                </div>
+
+            </div><!-- / row -->
+
+        </div><!-- / container -->
+
+    </section>
+
+    <?php include 'footer.php'; ?>
