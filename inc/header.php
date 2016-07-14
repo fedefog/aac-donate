84cf31df1c06caab1fc1146c54a90817008efe53
paddybalinia <paddybalinia@gmail.com>
2016-07-14 09:52:45 -0300

    - Estructura Dashboard

--- inc/header.php
+++ inc/header.php
@@ -9,15 +9,12 @@
         <title></title>        
 
         <!-- Bootstrap Core CSS -->
-        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
 
         <!-- Custom CSS -->
-        <link href='https://fonts.googleapis.com/css?family=Kite+One|Roboto+Condensed:400,300,700,300italic,400italic,700italic|Dosis:400,200,300,500,600,700,800|Satisfy' rel='stylesheet' type='text/css'>
+
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         
-        <link rel="stylesheet" href="css/jquery-ui.theme.min.css">
-        
-        <link rel="stylesheet" href="css/jquery-ui.structure.min.css"> 
 
         <link href="css/sections.css" rel="stylesheet">
 
--- index.php
+++ index.php
@@ -2,67 +2,108 @@
 include 'inc/header.php'
 ?>
 	
-<main id="home">
+<main id="dashboard">
 	
 	<div class="header-fixed">
 
         <header class="header ">
 
             <div class="container ">
-				
-				<h1>
+			
+				<div class="row">
 					
-					<a href="#">
-						
-						<img src="images/logo-aac.svg" alt="">
+					<div class="col-md-4">
+							
+						<h1>
+							
+							<a href="#">
+								
+								<img src="images/logo-aac.svg" alt="">
 
-					</a>
+							</a>
 
-				</h1>
-
-				 <a href="#" class="nav-mobile nav-icon4 visible-xs ">
-                                
-                    <span></span>
-                    <span></span>
-                    <span></span>
-
-                </a>
-				
-				<h2 class="title-welcome">Welcome to your account, <strong>David.</strong></h2>
-
-				<div class="box-account-header">
+						</h1>
 					
-					<div class="box-account">
-						
-						<h2 class="title">ACCOUNT</h2> 
-						
-						<h3 class="account-number">A7895</h3>
-						
-					</div><!-- /box-account -->
+					</div><!-- /col -->
 
-					<div class="box-balance">
-						
-						<h2 class="title">BALANCE</h2>
+					<div class="col-md-4">
+					
+						<h2 class="title-welcome">Welcome to your account, <strong>David.</strong></h2>
 
-						<h3 class="balance-number">£ 3,344.99</h3>
+						<div class="box-account-header">
+							
+							<div class="box-account">
+								
+								<h2 class="title">ACCOUNT</h2> 
+								
+								<h3 class="account-number">A7895</h3>
+								
+							</div><!-- /box-account -->
 
-					</div><!-- /box-balance -->
+							<div class="box-balance">
+								
+								<h2 class="title">BALANCE</h2>
 
-				</div><!-- /box-account-header -->
+								<h3 class="balance-number">£ 3,344.99</h3>
 
-				<h3 class="time">AS OF <strong>1 SEP 2016, 2:15PM</strong></h3>
+							</div><!-- /box-balance -->
 
+						</div><!-- /box-account-header -->
+
+						<h3 class="time">AS OF <strong>1 SEP 2016, 2:15PM</strong></h3>
+
+					</div><!-- /col -->	
+
+					<a href="#" class="nav-mobile nav-icon4 visible-xs ">
+	                                
+	                    <span></span>
+	                    <span></span>
+	                    <span></span>
+
+	                </a>
+				
+				</div><!-- /row  -->	
+            
             </div><!-- /container  -->
 
         </header>
 
     </div><!-- /header-fixed -->
 
-    <div class="box-daily-report">
-    	
+    <div class="container-fluid">
+	
+		<div class="row">
 		
+			<div class="col-xs-12">
+			   
+			    <div class="box-daily-updates">
+				    	
+					<a href="#" class="lkn-daily"><span class="date">SEP-14 </span>  ROSH HASHANAH UPDATE <i class="fa fa-angle-down" aria-hidden="true"></i></a>
 
-    </div><!-- /box-daily-report -->
+					<p class="text">The office will be closed Monday September 21 to Thursday the 24th. Please ensure all transactions are dealt with as soon as possible to avoid any issues given the high demand. Wishing everyone a ksiva v'chasima tova.</p>
+
+			    </div><!-- /box-daily-updates -->
+
+				<ul class="nav-dashboard">
+					<li class="dashboard-li">
+					<a href="#" class="lkn-dashboard">View Transactions <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+					</li>
+					<li class="dashboard-li">
+						<a href="#" class="lkn-dashboard">Make a Donation <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+					</li>
+					<li class="dashboard-li">
+						<a href="#" class="lkn-dashboard">View Standing orders <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+					</li>
+					<li class="dashboard-li">
+						<a href="#" class="lkn-dashboard">Order Vouchers Books <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+					</li>
+				</ul>
+			
+			</div><!-- /col -->
+		
+		</div><!-- /row -->
+		
+	</div><!-- /container-fluid -->
 
 </main>	
 	
