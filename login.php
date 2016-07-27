d962cd3eb7f41aecdf20aaa2a96cff748caf0e7f
Federico <fedefog@hotmail.com>
2016-07-27 18:14:12 -0300

    metas en la pagina de login

--- login.php
+++ login.php
@@ -4,8 +4,11 @@
         <meta charset="utf-8">
         
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title></title>        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, minimal-ui">
+        <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <title>AAC</title>        
 
         <!-- Bootstrap Core CSS -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
