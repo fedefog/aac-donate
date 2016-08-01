04234351545f2cf6e198eeab891e630bf51b8062
Federico <fedefog@hotmail.com>
2016-08-01 13:37:25 -0300

    pending page and icon

--- index.php
+++ index.php
@@ -12,6 +12,11 @@ include 'inc/header.php'
             <div class="container ">
 			
 				<div class="row">
+
+					<a href="transactions-pending.php" title="Pending Transactions" class="pending-bt">
+						<img src="images/pending-icon.png" width="21" height="21">
+						<span class="badge">2</span>
+					</a>
 					
 					<div class="col-md-4">
 							
