24535657e043df729bd5accfd406aa84e0ee712d
Federico <fedefog@hotmail.com>
2016-08-01 17:11:40 -0300

    back fade sorry by

--- transactions-emptystate.php
+++ transactions-emptystate.php
@@ -141,6 +141,12 @@ include 'inc/header.php'
 
 							<div class="container-sortby">
 
+								<a href="#" class="navigator-transactions-lkn lkn-sortby">
+							
+									<i class="fa fa-sort-amount-desc" aria-hidden="true"></i>
+
+								</a>
+
 								<ul class="list-sortby">
 									<li class="sortby-li">
 										<h2 class="title-sortby">SORT BY</h2>
@@ -288,6 +294,7 @@ include 'inc/header.php'
 
 </div><!-- /modal-search -->
 
+<div class="modal-backdrop fade sort-back"></div>
 
 <? include 'inc/online-donation-modal.php' ?>
 <? include 'inc/give-as-you-earn-modal.php' ?>
