0277f02148f24692e1f0acd8bec16e864de7d689
paddybalinia <paddybalinia@gmail.com>
2016-08-16 17:15:58 -0300

    Help angosto

--- help.php
+++ help.php
@@ -75,7 +75,7 @@
 
 				</div><!-- /col -->
 
-				<div class="col-xs-12 col-md-8 col-md-offset-2">
+				<div class="col-xs-12 col-md-8 col-md-offset-2 col-md-6 col-md-offset-3">
 
 					<div class="box-faqs-header hidden-xs">
 		
