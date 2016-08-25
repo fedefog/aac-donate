42ff5a28e394a75705fe9f3657dd2a65e18edbc8
Federico <fedefog@hotmail.com>
2016-08-25 12:44:07 -0300

    adjustment beneficiary field.

--- make-a-donation.php
+++ make-a-donation.php
@@ -124,9 +124,9 @@
 
 							 <input type="text" name="q" placeholder="Type your search" />
 							 <ul class="results" >
-								 <li><a href="#">The Riding London NW11 8HL<br /><span>GGBH</span></a></li>
-								 <li><a href="#">The Riding London NW11 8HL<br /><span>GGBH</span></a></li>
-						 		<li><a href="#">The Riding London NW11 8HL<br /><span>GGBH</span></a></li>
+								 <li><a href="#"><span>GGBH</span><br />The Riding London NW11 8HL</a></li>
+								 <li><a href="#"><span>GGBH</span><br />The Riding London NW11 8HL</a></li>
+						 		<li><a href="#"><span>GGBH</span><br />The Riding London NW11 8HL</a></li>
 							 </ul>
 						 </form>
 						
