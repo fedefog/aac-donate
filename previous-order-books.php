b8340cf9836333b88788f4957cea73c0239ec4cc
paddybalinia <paddybalinia@gmail.com>
2016-12-02 10:41:50 -0300

    - Voucher books

--- previous-order-books.php
+++ previous-order-books.php
@@ -11,37 +11,86 @@
         <tbody>
             <tr class=" order-books-row">
                 <td class="modal-show" data-toggle="modal" data-target="#modal-voucher">
-                            <a href="javascript:void(0);">
-                                <div class="date">23-11-16</div>
-                            </a>
-                        </td>
-                        <td class="modal-show" data-toggle="modal" data-target="#modal-voucher">
-                            <a href="javascript:void(0);">
-                                <span class="span-request-id">
-                                    
-                                        46184
-                                    
-                                </span><!-- /desc-table -->
-                            </a>
-                        </td>
-                        <td class="modal-show" data-toggle="modal" data-target="#modal-voucher">
-                            <a href="javascript:void(0);">
-                               <span class="info-vocher-book-orderer">5 x Pre-Printed £3
-                                <br>
-                                <spam class="being-processed">CURRENTLY BEING PROCESSED</spam>
-                                </span>
-                            </a>
-                        </td>
-                        <td class="modal-show amount-td hidden-xs" data-toggle="modal" data-target="#modal-voucher">
-                            <a href="javascript:void(0);">
-                                <span class="amount">
-                                    £0.00
-                                </span>
-                            </a>
-                        </td>
+                    <a href="javascript:void(0);">
+                        <div class="date">23-11-16</div>
+                    </a>
+                </td>
+                <td class="modal-show" data-toggle="modal" data-target="#modal-voucher">
+                    <a href="javascript:void(0);">
+                        <span class="span-request-id">
+                            
+                                46184
+                            
+                        </span><!-- /desc-table -->
+                    </a>
+                </td>
+                <td class="modal-show" data-toggle="modal" data-target="#modal-voucher">
+                    <a href="javascript:void(0);">
+                       <span class="info-vocher-book-orderer">5 x Pre-Printed £3
+                        <br>
+                        <spam class="being-processed">CURRENTLY BEING PROCESSED</spam>
+                        </span>
+                    </a>
+                </td>
+                <td class="modal-show amount-td hidden-xs" data-toggle="modal" data-target="#modal-voucher">
+                    <a href="javascript:void(0);">
+                        <span class="amount">
+                            £0.00
+                        </span>
+                    </a>
+                </td>
                         
                         
-                </tr>
-                                            
+            </tr>
+            <tr class=" order-books-row">
+                <td class="modal-show" data-toggle="modal" data-target="#modal-voucher">
+                    <a href="javascript:void(0);">
+                        <div class="date">23-11-16</div>
+                    </a>
+                </td>
+                <td class="modal-show" data-toggle="modal" data-target="#modal-voucher">
+                    <a href="javascript:void(0);">
+                        <span class="span-request-id">
+                            
+                                46184
+                            
+                        </span><!-- /desc-table -->
+                    </a>
+                </td>
+                <td class="modal-show" data-toggle="modal" data-target="#modal-voucher">
+                    <a href="javascript:void(0);">
+                       <span class="info-vocher-book-orderer">2 x £1 Pre-Paid Voucher Book, 3 x 50p Pre-Paid Voucher Book, 5 x Pre-Printed £10
+                        
+                        
+                        </span>
+                    </a>
+                </td>
+                <td class="modal-show amount-td hidden-xs" data-toggle="modal" data-target="#modal-voucher">
+                    <a href="javascript:void(0);">
+                        <span class="amount">
+                            £0.00
+                        </span>
+                    </a>
+                </td>
+                        
+                        
+            </tr>
+
+            <tr class="order-books-row">
+                
+                <td colspan="4">
+                    
+                    <div class="box-empty-state">
+                            
+                        <span class="title-empty-state">You have not yet ordered any voucher books.</span>
+
+                        <a href="#" class="lkn-oreder-voucher">ORDER VOUCHER BOOKS</a>
+                        
+                    </div>
+                    
+                </td>
+
+            </tr>
+
         </tbody>
     </table>        
